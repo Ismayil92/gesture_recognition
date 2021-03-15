f87f3289815decaaf906f95470216d5d94a0ae34
Ismayil Ahmadli <38913625+Ismayil92@users.noreply.github.com>
2021-03-15 16:41:42 +0100

    Update parse_arguments.py

--- parse_arguments.py
+++ parse_arguments.py
@@ -1,3 +1,24 @@
+'''
+/* ===========================================================================
+** Copyright (C) 2019 Infineon Technologies AG. All rights reserved.
+** ===========================================================================
+**
+** ===========================================================================
+** Infineon Technologies AG (INFINEON) is supplying this file for use
+** exclusively with Infineon's sensor products. This file can be freely
+** distributed within development tools and software supporting such 
+** products.
+** 
+** THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+** OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+** MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+** INFINEON SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR DIRECT, INDIRECT, 
+** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES, FOR ANY REASON 
+** WHATSOEVER.
+** ===========================================================================
+*/
+'''
+
 import argparse  
 
 def parse_opts():
